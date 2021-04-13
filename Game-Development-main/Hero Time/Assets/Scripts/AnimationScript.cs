87d1ca33024bfb58217606aaf60101dd89f3b319
Laiba-Arooj <70106509+Laiba-Arooj@users.noreply.github.com>
2021-04-13 13:27:55 +0500

    Update Script
    Animation Script Modified

--- Game-Development-main/Hero Time/Assets/Scripts/AnimationScript.cs
+++ Game-Development-main/Hero Time/Assets/Scripts/AnimationScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class AnimationScript : MonoBehaviour
 {
+    public int speed = 3;
+    public int js = 10;
+    public new Rigidbody2D rigidbody;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mov = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", mov * speed);
+        float movVertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector3(mov * speed, movVertical * speed, 0);
+        float runspeed = Input.GetAxis("Jump");
+        Debug.Log(runspeed);
+        if (runspeed >=1)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, js, 0);
+            animator.SetBool("isJump", true);
+        }
+        else if (runspeed < 0)
+        {
+            animator.SetBool("isJump", false);
+        }
     }
 }
